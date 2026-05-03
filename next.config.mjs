45adc5c25087fcb54fb619f34c008428996d02cb
sethum-VS <smwopendev@gmail.com>
2026-05-03 11:00:49 +0530

    Update next.config.mjs with withWorkflow wrapper and install workflow packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+import { withWorkflow } from '@upstash/workflow';
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withWorkflow(nextConfig);
